60906f2d4a5ff8d2c7000585b88c7f029b165bf6
Admin <lacou.alban@gmail.com>
2020-12-01 19:41:36 +0100

    push de fin de journée

--- imagefilter/image.py
+++ imagefilter/image.py
@@ -4,17 +4,14 @@ import sys
 
 def open_image():
 
-
+        
         image = cv2.imread('amongus.jpeg')
+        print("l'image est ouverte")
         return image
-
-
-
-
-
 def nb(image):
     try:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        print("l'image est noir est blanc")
         return gray
     except:
         print("l'image n'est pas bonne")
@@ -24,7 +21,7 @@ def blur(image):
 
         ksize = (10, 10)
         blur = cv2.blur(image, ksize)
-
+        print("l'image est floue")
         return blur
     except:
         print("l'image n'est pas bonne ou la valeur du flou est negative")
@@ -33,15 +30,14 @@ def dilate(image):
     try:
         kernel = np.ones((5, 5), np.uint8)
         dilation = cv2.dilate(image,kernel ,20)
+        print("l'image est dilate")
         return dilation
     except:
         print("l'image n'est pas bonne")
         sys.exit()
 def save(imageToSave):
     cv2.imwrite('imagesvg/image.jpeg', imageToSave)
+    print("l'image est sauvegarder")
 
 
 
-image = open_image()
-dilatation = blur(image)
-save(dilatation)
--- imagefilter/main.py
+++ imagefilter/main.py
@@ -1,2 +1,23 @@
 import cv2
+import sys
+import image as i
+
+args = sys.argv
+image = ""
+
+if args[1] == "open":
+    image = i.open_image()
+    if args[2] == "nb":
+        image = i.nb(image)
+        i.save(image)
+    elif args[2] == "blur":
+        image = i.blur(image)
+        i.save(image)
+    elif args[2] == "dilate":
+        image = i.dilate(image)
+        i.save(image)
+if args[1] == "--filter":
+    option = args[2]
+    option = option.split("|")
+
 
