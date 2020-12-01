8065f35584f1879bae846a7c04bae7c1c2f5fde6
Admin <lacou.alban@gmail.com>
2020-12-01 16:00:28 +0100

    story 1 (image en noir et blanc

--- imagefilter/image.py
+++ imagefilter/image.py
@@ -1,3 +1,11 @@
 import cv2
 
-image = cv2.imread('')
+def open_image():
+    image = cv2.imread('helloWorld.png')
+    cv2.imshow('Original image',image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Original image', gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.imwrite('imagesvg/image.jpeg', gray)
+open_image()
