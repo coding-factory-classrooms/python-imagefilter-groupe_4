66e2919b08cca3ded47dfa5e6cd8645be6165549
Admin <lacou.alban@gmail.com>
2020-12-01 16:54:29 +0100

    ajout de la gestion des erreurs

--- imagefilter/image.py
+++ imagefilter/image.py
@@ -1,28 +1,47 @@
 import cv2
 import numpy as np
+import sys
 
 def open_image():
-    image = cv2.imread('amongus.jpeg')
-    return image
+
+
+        image = cv2.imread('amongus.jpeg')
+        return image
+
+
 
 
 
 def nb(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return gray
+    try:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return gray
+    except:
+        print("l'image n'est pas bonne")
+        sys.exit()
 def blur(image):
-    ksize = (10, 10)
-    blur = cv2.blur(image, ksize)
-    return blur
+    try:
+
+        ksize = (10, 10)
+        blur = cv2.blur(image, ksize)
+
+        return blur
+    except:
+        print("l'image n'est pas bonne ou la valeur du flou est negative")
+        sys.exit()
 def dilate(image):
-    kernel = np.ones((5, 5), np.uint8)
-    dilation = cv2.dilate(image,kernel ,20)
-    return dilation
+    try:
+        kernel = np.ones((5, 5), np.uint8)
+        dilation = cv2.dilate(image,kernel ,20)
+        return dilation
+    except:
+        print("l'image n'est pas bonne")
+        sys.exit()
 def save(imageToSave):
     cv2.imwrite('imagesvg/image.jpeg', imageToSave)
 
 
 
 image = open_image()
-dilatation = dilate(image)
+dilatation = blur(image)
 save(dilatation)
