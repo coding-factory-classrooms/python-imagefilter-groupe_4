eb512465ae3ac466e912e18d1a3c853e3cb2449f
Admin <lacou.alban@gmail.com>
2020-12-01 16:23:01 +0100

    ajout du flou + separation en plusieurs fonction

--- imagefilter/image.py
+++ imagefilter/image.py
@@ -2,10 +2,22 @@ import cv2
 
 def open_image():
     image = cv2.imread('helloWorld.png')
-    cv2.imshow('Original image',image)
+    return image
+
+
+
+def nb(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Original image', gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imwrite('imagesvg/image.jpeg', gray)
-open_image()
+    return gray
+def blur(image):
+    ksize = (10, 10)
+    blur = cv2.blur(image, ksize)
+    return blur
+def save(imageToSave):
+    cv2.imwrite('imagesvg/image.jpeg', imageToSave)
+
+
+
+image = open_image()
+blur = blur(image)
+save(blur)
